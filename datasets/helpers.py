351fb96d596341b6a711782e12a93c2327dd69b3
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2019-11-20 18:22:27 -0300

    refactor

---
+++ datasets/helpers.py
@@ -0,0 +1,61 @@
+from PIL import Image, ImageStat
+from PIL.ImageDraw import ImageDraw
+
+def random_point(w, h, minx=0, miny=0):
+    x, y = randint(minx, w), randint(miny, h)
+    return (x, y)
+
+def area_ellipse(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    x_len = abs(x1 - x2)
+    y_len = abs(y1 - y2)
+    return pi * x_len * y_len
+
+def gen_example(w, h, num_polygons=1, count=False):
+    example = Image.new('1', (w, h))
+    draw = ImageDraw(example, mode='1')
+
+    total_area = w * h
+    area = 0
+    for i in range(num_polygons):
+        p1 = random_point(w, h)
+        p2 = random_point(w, h, p1[0], p1[1])
+        draw.rectangle([p1, p2], fill=1)
+
+    (area, ) = ImageStat.Stat(example).sum
+    if not count:
+        area /= total_area
+    return (example, area)
+
+def gen_df(
+        root_dir, 
+        dt_len, 
+        img_size=(512, 512), 
+        skip=True, 
+        test=False, 
+        count=False):
+    root_dir = path.abspath(root_dir)
+    root_dir = path.join(root_dir, "test" if test else "train")
+    img_dir = path.join(root_dir, 'images')
+    df_dest = path.join(root_dir, 'data.csv')
+    if path.exists(df_dest) and skip:
+        print("Found existing dataset, skipping...")
+        return pd.read_csv(df_dest, index_col=0)
+    
+    for directory in [root_dir, img_dir]:
+        if not path.exists(directory):
+            os.makedirs(directory)
+            print(f"Created directory {directory}")
+
+    df = pd.DataFrame(columns=['filename', 'area'])
+    for i in range(dt_len):
+        filename = f"img_{i}.jpeg"
+        dest_path = path.join(img_dir, filename)
+        img, area = gen_example(*img_size, count=count)
+        img.save(dest_path)
+        row = pd.Series({"filename": filename, "area": area})
+        df.loc[i] = row
+
+    df.to_csv(df_dest)
+    return df
---
+++ models/models.py
@@ -0,0 +1,33 @@
+from torch import nn
+
+def base_model(max_channels, activation_fn, pool_layer, pool_size, num_layers, conv_stride=1):
+    return nn.Sequential(
+        *[base_layer({
+            "in_channels": i if i < max_channels else max_channels, 
+            "out_channels": abs(i+1) if i < max_channels else max_channels,
+            "kernel_size": 3,
+            "stride": conv_stride
+            }, pool_layer, pool_size, activation_fn) for i in range(1, num_layers//2 + 1)],
+        *[base_layer({
+            "in_channels": i if i < max_channels else max_channels,
+            "out_channels": i - 1 if i - 1 < max_channels else max_channels,
+            "kernel_size": 3,
+            "stride": conv_stride 
+        }, pool_layer, pool_size, activation_fn) for i in range(num_layers//2 + 1, 1, -1)]
+    )
+
+default_params = {
+    "max_channels": 3,
+    "activation_fn": nn.ReLU,
+    "pool_size": 2,
+    "num_layers": 4,
+    "conv_stride": 1
+}
+
+def get_models(params=default_params):
+    return [
+        {"model": base_model(pool_layer=SumPool2d, **params), "name": "sum_pool"},
+        {"model": base_model(pool_layer=nn.MaxPool2d, **params), "name": "max_pool"},
+        {"model": base_model(pool_layer=nn.AvgPool2d, **params), "name": "avg_pool"}
+    ]
+
