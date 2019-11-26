6eaec367e11bd5c15c4b01ee78641293c9e014e0
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2019-11-26 17:36:46 -0300

    fastai nao aceita treino entao era inutil

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -8,16 +8,10 @@ def gen_dataset_options(
         device, 
         transform_args,
         train_dir='train', 
-        test_dir='test', 
         csv='data.csv'):
     return {
         "train_dir": path.join(root_dir, 'train', 'images'),
-        "test_dir": path.join(root_dir, 'test', 'images'),
         "df_train": pd.read_csv(
-                path.join(root_dir, 'train', 'data.csv'),
-                index_col=0
-            ),
-        "df_test": pd.read_csv(
                 path.join(root_dir, 'train', 'data.csv'),
                 index_col=0
             ),
@@ -27,15 +21,11 @@ def gen_dataset_options(
     }
     
 
-def get_dataset(train_dir, test_dir, df_train, df_test, bs, device, transform_args):
-    test_item_list = (ImageList
-        .from_df(path=test_dir, df=df_test, convert_mode='1')
-    )
+def get_dataset(train_dir, df_train, bs, device, transform_args):
     return (ImageList
             .from_df(path=train_dir, df=df_train, convert_mode='1')
             .split_by_rand_pct()
             .label_from_df(cols=1, label_cls=FloatList)
             .transform(**transform_args)
-            .add_test(test_item_list)
             .databunch(bs=bs, device=device)  
         )
