7be2e33717d5b947c937203a6b26ae70c3c27b37
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2019-11-28 17:32:18 -0300

    jupytext

--- PolygonDatasetGenerator.py
+++ PolygonDatasetGenerator.py
@@ -13,60 +13,28 @@
 # ---
 
 # +
-from PIL import Image, ImageStat
-from PIL.ImageDraw import ImageDraw
-
-import pandas as pd
-from matplotlib import pyplot as plt
-import torch
-from torch import nn
-from torch.nn import functional as F
-
-from random import randint
-from math import pi
-
-import os
+from tqdm import tqdm
 from os import path
 
-from fastai import vision
-from fastai.vision import ImageList, FloatList, cnn_learner, pil2tensor, open_image
-from fastai.callbacks import EarlyStoppingCallback, ReduceLROnPlateauCallback, CSVLogger
-from fastai.train import ShowGraph, Learner
-from fastai.metrics import mean_absolute_error, mean_squared_error
-from torchvision import models
-from torch import nn
-import numpy as np
-
-from functools import partial
-
 ## Local Imports ##
-from models import helpers as model_helpers, models as custom_models
 from datasets import helpers as dataset_helpers
 
 # + [markdown] colab_type="text" id="QLVyf1-umCV2"
 # ## Dataset
 
 # +
-DT_LEN = 25000
-COUNT = True
+DT_LEN = 15000
 ROOT_DIR = 'data'
-DT_ROOT_DIR = 'polygon_data_counts' if COUNT else 'polygon_data'
-DT_ROOT_DIR = path.join(ROOT_DIR, DT_ROOT_DIR)
-DT_ROOT_DIR = path.abspath(DT_ROOT_DIR)
-MODEL_ROOT_DIR = path.join(DT_ROOT_DIR, 'results')
-W, H = 512, 512
-BATCH_SIZE = 64
+W, H = 256, 256
 
 DATASETS = [
-        {"root_dir": path.join(ROOT_DIR, "polygon_data_counts"), "count": True, "skip": True},
-        {"root_dir": path.join(ROOT_DIR, "polygon_data_percentage"), "count": False, "skip": True},
+        {"root_dir": path.join(ROOT_DIR, "polygon_data_counts"), "count": True, "skip": False},
+        {"root_dir": path.join(ROOT_DIR, "polygon_data_percentage"), "count": False, "skip": False},
         {"root_dir": path.join(ROOT_DIR, "ellipse_data_counts"), "count": True, "skip": False, "draw_polygon_fn":dataset_helpers.draw_ellipse},
         {"root_dir": path.join(ROOT_DIR, "ellipse_data_percentage"), "count": False, "skip": False, "draw_polygon_fn":dataset_helpers.draw_ellipse}
     ]
 
-for dataset in DATASETS:
-    df_train = dataset_helpers.gen_df(dt_len=DT_LEN, test=False, **dataset)
-    df_test  = dataset_helpers.gen_df(dt_len=DT_LEN//2, test=True, **dataset)
-# -
-
-
+for dataset in tqdm(DATASETS):
+    df_train = dataset_helpers.gen_df(dt_len=DT_LEN, test=False, img_size=(W, H), **dataset)
+    df_test  = dataset_helpers.gen_df(dt_len=DT_LEN//2, test=True, img_size=(W, H), **dataset)
+print("DONE!")
--- train.py
+++ train.py
@@ -20,7 +20,11 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import torch
 from torch import nn
+from torch.utils import data
 from torch.nn import functional as F
+
+import torchvision
+from torchvision import transforms
 
 from random import randint
 from math import pi
@@ -28,169 +32,136 @@ from math import pi
 import os
 from os import path
 
-from fastai import vision
-from fastai.vision import ImageList, FloatList, cnn_learner, pil2tensor, open_image
-from fastai.callbacks import EarlyStoppingCallback, ReduceLROnPlateauCallback, CSVLogger
-from fastai.train import ShowGraph, Learner
-from fastai.metrics import mean_absolute_error, mean_squared_error
-from torchvision import models
-from torch import nn
 import numpy as np 
-
-from functools import partial
+import math
+from functools import partial, reduce
 import itertools
 from collections import namedtuple
+
+from tqdm import tqdm
 
 ## Local Imports ##
 from models import helpers as model_helpers, models as custom_models
 from datasets import helpers as dataset_helpers, datasets as custom_datasets
 
 # +
-W, H = (512, 512)
-BS = 256
-DEVICE = 'cuda'
+W, H = (256, 256)
+BS = 128
+MAX_EPOCHS = 100
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 DT_ROOT = 'data'
-POLYGON_COUNT_DIR = 'polygon_data_counts'
-POLYGON_PERCENTAGE_DIR = 'polygon_data_percentage'
-ELLIPSE_COUNT_DIR = 'ellipse_data_counts'
-ELLIPSE_PERCENTAGE_DIR = 'ellipse_data_percentage'
-
-options = {
-    "bs": BS,
-    "device": DEVICE,
-    "transform_args": {"size": (W, H)}
-}
-
-rect_counts_options = custom_datasets.gen_dataset_options(
-    root_dir=path.join(DT_ROOT, POLYGON_COUNT_DIR),
-    **options
-)
-
-rect_percentage_options = custom_datasets.gen_dataset_options(
-    root_dir=path.join(DT_ROOT, POLYGON_PERCENTAGE_DIR),
-    **options
-)
-
-ellipse_counts_options = custom_datasets.gen_dataset_options(
-    root_dir=path.join(DT_ROOT, ELLIPSE_COUNT_DIR),
-    **options
-)
-
-ellipse_percentage_options = custom_datasets.gen_dataset_options(
-    root_dir=path.join(DT_ROOT, ELLIPSE_PERCENTAGE_DIR),
-    **options
-)
+POLYGON_COUNT_DIR = path.join(DT_ROOT, 'polygon_data_counts')
+POLYGON_PERCENTAGE_DIR = path.join(DT_ROOT, 'polygon_data_percentage')
+ELLIPSE_COUNT_DIR = path.join(DT_ROOT, 'ellipse_data_counts')
+ELLIPSE_PERCENTAGE_DIR = path.join(DT_ROOT, 'ellipse_data_percentage')
+TRANSFORM = transforms.Compose([transforms.ToTensor()])
 
 dataset_generators = [
-    model_helpers.Param('RECT_COUNT', lambda: custom_datasets.get_dataset(**rect_counts_options)),
-    model_helpers.Param('RECT_PCT', lambda: custom_datasets.get_dataset(**rect_percentage_options)),
-    model_helpers.Param('ELLIPSE_COUNT', lambda: custom_datasets.get_dataset(**ellipse_counts_options)),
-    model_helpers.Param('ELLIPSE_PCT', lambda: custom_datasets.get_dataset(**ellipse_percentage_options))
+    model_helpers.Param('RECT_COUNT', custom_datasets.get_dataset(
+        root_dir=POLYGON_COUNT_DIR, 
+        df_path=path.join(POLYGON_COUNT_DIR, 'data.csv'),
+        transform=TRANSFORM,
+        bs=BS
+    )),
+    model_helpers.Param('RECT_PCT', custom_datasets.get_dataset(
+        root_dir=POLYGON_PERCENTAGE_DIR, 
+        df_path=path.join(POLYGON_PERCENTAGE_DIR, 'data.csv'),
+        transform=TRANSFORM,
+        bs=BS
+    )),
+    model_helpers.Param('ELLIPSE_COUNT', custom_datasets.get_dataset(
+        root_dir=ELLIPSE_COUNT_DIR, 
+        df_path=path.join(ELLIPSE_COUNT_DIR, 'data.csv'),
+        transform=TRANSFORM,
+        bs=BS
+    )),
+    model_helpers.Param('ELLIPSE_PCT', custom_datasets.get_dataset(
+        root_dir=ELLIPSE_PERCENTAGE_DIR, 
+        df_path=path.join(ELLIPSE_PERCENTAGE_DIR, 'data.csv'),
+        transform=TRANSFORM,
+        bs=BS
+    ))
 ]
 
+models_to_test = [
+    *custom_models.get_models(input_size=(1, W, H)), 
+    model_helpers.Param('PERCEPTRON', lambda: nn.Sequential(model_helpers.Flatten(), nn.Linear(W * H, 1)))
+]
+
+print(f"Device: {DEVICE}")
+
 # +
-EARLY_STOP_PATIENCE = 100
-REDUCE_ON_PLATEAU_PATIENCE = 30 
-MAX_EPOCHS = 100
-
-learner_args = {
-    "metrics": [
-        mean_squared_error,
-        mean_absolute_error
-    ],
-    "callback_fns": [
-            #partial(EarlyStoppingCallback, patience=EARLY_STOP_PATIENCE), 
-            partial(ReduceLROnPlateauCallback, patience=REDUCE_ON_PLATEAU_PATIENCE),
-            partial(CSVLogger, filename=f"history_mlp")
-        ],
-    "silent": True
-}
-
 OPTIMS = [
-    #model_helpers.Param('Adam', torch.optim.Adam),
-    model_helpers.Param('SGD', torch.optim.SGD)
+    model_helpers.Param('Adam', lambda: torch.optim.Adam),
+    model_helpers.Param('SGD', lambda: partial(torch.optim.SGD, lr=0.00001))
 ]
 
 LOSS_FNS = [
-    model_helpers.Param('L1LOSS', nn.L1Loss),
-    #model_helpers.Param('MSELOSS', nn.MSELoss)
+    model_helpers.Param('L1LOSS', lambda: model_helpers.squeeze_loss(nn.L1Loss())),
+    model_helpers.Param('MSELOSS', lambda: model_helpers.squeeze_loss(nn.MSELoss()))
 ]
 
-models_to_test = custom_models.get_models()
 
-cnn_grid = model_helpers.new_grid_search(models_to_test, OPTIMS, LOSS_FNS)
+grid = model_helpers.new_grid_search(models_to_test, OPTIMS, LOSS_FNS)
+grid = list(grid)
 
-mlp_grid = model_helpers.new_grid_search([None], OPTIMS, LOSS_FNS)
-cnn_grid, mlp_grid = list(cnn_grid), list(mlp_grid)
-print(f"Will train {(len(cnn_grid) + len(mlp_grid)) * len(dataset_generators)} models")
-## WHAT I DO WHITH DISSSS????
-squeeze_loss = lambda loss_fn: lambda x,y: loss_fn(x.view(-1), y)
-# -
-
-df_results = pd.DataFrame(columns=["pth", "dt", "loss", "optim", "val_loss"])
+print(f"Will train {len(LOSS_FNS) * len(models_to_test) * len(dataset_generators)} models")
+print(f"{len(models_to_test)} MODELS")
+print(f"{len(dataset_generators)} DATASETS")
+print(f"{len(LOSS_FNS)} LOSS_FNS")
+print(f"{len(OPTIMS)} OPTIMS")
 
 
 # +
-class Flatten(nn.Module):
-    def forward(self, x):
-        return x.view(x.size(0), -1)
+def normalize_train_test_df(df, indexes):
+    train_losses = (pd.DataFrame(df.train_losses.tolist(), index=df.set_index(indexes).index).stack()
+        .reset_index(name='train_losses'))
+    val_losses = (pd.DataFrame(df.val_losses.tolist(), index=df.set_index(indexes).index).stack()
+        .reset_index(name='val_losses'))
+    epoch = (pd.DataFrame(df.epoch.tolist(), index=df.set_index(indexes).index).stack()
+        .reset_index(name='epoch'))
+    val_losses["epoch"] = epoch.epoch
+    val_losses["train_losses"] = train_losses.train_losses
+    return val_losses.drop(columns="level_4").set_index(indexes)
 
-for dataset in dataset_generators:
-    bunch = dataset.param()
-    for row in mlp_grid:
-        print(f"Training {dataset.name}, {row.opt.name}, {row.loss.name}")
-        model_path = path.join(bunch.path,'..', '..', 'results', 'mlp', row.opt.name, row.loss.name)
-        model_path = path.abspath(model_path)
-        learn = Learner(
-            model=nn.Sequential(Flatten(), nn.Linear(W * H, 1)),
-            opt_func=row.opt.param,
-            data=bunch,
-            loss_func=squeeze_loss(row.loss.param()),
-            path=model_path,
-            **learner_args
-        )
-        learn.fit(MAX_EPOCHS)
-        model_helpers.save_stats(learn)
-        learn.save('model')
-        df_results = df_results.append({
-                "pth": learn.path, 
-                "dt": dataset.name,
-                "loss": row.loss.name, 
-                "optim": row.opt.name, 
-                "val_loss": learn.recorder.losses[-1].item()
-            }, ignore_index=True)
+
+indexes = [
+        "model_name",
+        "dataset",
+        "optim",
+        "loss_fn"
+]
+
+columns = [
+        *indexes,
+        "train_losses", 
+        "val_losses"]
+
+df = pd.DataFrame(
+    columns=columns
+)
+
+for dt in dataset_generators:
+    dl_train = dt.param.train()
+    dl_test = dt.param.test()
+    for row in grid:
+        print(f"Pool: {row.model.name}\nOpt: {row.opt.name}\nLoss: {row.loss.name}\nDT: {dt.name}\n")
+        metrics = model_helpers.train(
+            dl_train, 
+            dl_test, 
+            row.opt.param(),
+            row.loss.param(), 
+            row.model.param(), 
+            MAX_EPOCHS, 
+            DEVICE)
+        row = pd.Series({
+            "model_name": row.model.name,
+            "dataset": dt.name,
+            "optim": row.opt.name,
+            "loss_fn": row.loss.name
+        }).append(pd.Series(metrics))
+        df = df.append(row, ignore_index=True)
 # -
 
-for dataset in dataset_generators:
-    bunch = dataset.param()
-    for row in cnn_grid:
-        print(f"Training {dataset.name}, {row.model.name}, {row.opt.name}, {row.loss.name}")
-        model_path = path.join(bunch.path, '..', '..', 'results', row.model.name, row.opt.name, row.loss.name)
-        model_path = path.abspath(model_path)
-        learn = cnn_learner(
-            data=bunch,
-            path=model_path,
-            base_arch=lambda t: row.model.param,
-            cut=lambda x: x,
-            loss_func=squeeze_loss(row.loss.param()),
-            opt_func=row.opt.param,
-            **learner_args
-        )
-        learn.fit(MAX_EPOCHS)
-        model_helpers.save_stats(learn)
-        learn.save('model')
-        df_results = df_results.append({
-                "pth": learn.path, 
-                "dt": dataset.name,
-                "loss": row.loss.name, 
-                "optim": row.opt.name, 
-                "val_loss": learn.recorder.losses[-1].item()
-            }, ignore_index=True)
-
-df_results.to_csv('FULL_RESULTS.CSV')
-
-# +
-## Broken... ## 
-#DT_ROOT_DIRS = map(lambda p: path.join(DT_ROOT, p, 'results'),[POLYGON_COUNT_DIR, POLYGON_PERCENTAGE_DIR])
-#for root_dir in DT_ROOT_DIRS:
-#    model_helpers.summarize_results([*models_to_test, {"name":"mlp"}], root_dir)
+df = normalize_train_test_df(df, indexes)
+df.to_csv('FULL_RESULTS.csv')
